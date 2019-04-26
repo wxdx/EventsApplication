e43ee5b5fc92b0ed9b1ffcef2e3eb3ede074c5f1
wangxiaodong <lle@live.com>
2019-04-27 00:46:16 +0800

    修改

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 	resources :events
-	root "/events"
 	get "/welcome" => "welcome#index"
 	#match ':controller(/:action(/:id(.:format)))', :via => :all
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
