52eeec17966d00ed4e0bd432efeb0797cac9bcff
wangxiaodong <lle@live.com>
2019-04-26 23:51:04 +0800

    路由风格修改成RESTful

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,13 @@
 class EventsController < ApplicationController
 	before_action :set_event, :only =>[:show, :edit, :update, :destroy]
 	#GET /events/index
+	#GET /events
 	def index
 		@events = Event.page(params[:page]).per(10)
+	end
+
+	#GET /events/:id
+	def show
 	end
 
 	#GET /events/new
@@ -10,40 +15,38 @@ class EventsController < ApplicationController
 		@event = Event.new
 	end
 
-	#POST /events/create
+	#POST /events
 	def create
 		@event = Event.new(event_params)
 		if @event.save
 			flash[:notice] = "新增成功"
-			redirect_to :action => :index
+			redirect_to events_path
 		else
 			render :action => :new
 		end
 	end
 
-	#GET /events/show/:id
-	def show
-	end
 
-	#GET /events/edit/:id
+
+	#PATCH /events/:id/edit
 	def edit
 	end
 
-	#POST /events/update/:id
+	#PATCH /events/:id
 	def update
 		if @event.update(event_params)
 			flash[:notice] = "更新成功"
-			redirect_to :action => :show, :id => @event
+			redirect_to event_path(@event)
 		else 
 			render :action => :edit
 		end	
 	end
 
-	#POST /events/destory/:id
+	#DELETE /events/:id
 	def destroy
 		@event.destroy
 		flash[:alert] = "删除成功"
-		redirect_to :action => :index
+		redirect_to events_path
 	end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+	resources :events
 	get "/welcome" => "welcome#index"
-	match ':controller(/:action(/:id(.:format)))', :via => :all
+	#match ':controller(/:action(/:id(.:format)))', :via => :all
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
