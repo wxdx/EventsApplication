16639ebe0947135c2df2e992f783f918f24ea845
wangxiaodong <lle@live.com>
2019-04-29 00:19:37 +0800

    add root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 	resources :events
 	get "/welcome" => "welcome#index"
+	root "events#index"
 	#match ':controller(/:action(/:id(.:format)))', :via => :all
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
